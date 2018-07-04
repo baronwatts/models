dc63f94e0513f5fa58223ed5c1709f4b2f1fb038
Baron Watts <watts.baron@hotmail.com>
2018-07-03 22:04:38 -0700

    Add files via upload

---
+++ blade-of-grass.js
@@ -0,0 +1,46 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "blade-of-grass.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 5,
+        "faces"         : 3,
+        "normals"       : 5,
+        "colors"        : 0,
+        "uvs"           : 5,
+        "materials"     : 1
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "grass_02",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.64, 0.64, 0.64],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "grass_d.tga",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 94.117647,
+	"transparency" : 1.0
+	}],
+
+    "vertices": [0.116776,1.511328,-0.136709,0.036393,0.853477,-0.081320,0.090555,0.853053,-0.044954,0.070898,-0.011792,-0.003517,0.016736,-0.011863,-0.036834],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [-0.5515,0.1367,0.8229,-0.5544,0.0956,0.8268,-0.5442,0.0816,0.835,-0.5233,0.0526,0.8505,-0.5399,0.054,0.84],
+
+    "colors": [],
+
+    "uvs": [[0.5,1,0.1836,0.7038,0.832,0.6999,0.821,0.0039,0.1946,0.0078]],
+
+    "faces": [42,0,1,2,0,0,1,2,0,1,2,42,3,2,4,0,3,2,4,3,2,4,42,2,1,4,0,2,1,4,2,1,4]
+
+}
