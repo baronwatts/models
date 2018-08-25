8e0b58405cae1dd4d5e18f74c8e90a993ce1008e
Baron Watts <watts.baron@hotmail.com>
2018-08-24 18:10:23 -0700

    Add files via upload

---
+++ pink-leaf.js
@@ -0,0 +1,46 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "pink-leaf.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 4,
+        "faces"         : 2,
+        "normals"       : 2,
+        "colors"        : 1,
+        "uvs"           : 0,
+        "materials"     : 1
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "???????_??????",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.6393, 0.041, 0.1727],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 31.372549,
+	"transparency" : 1.0,
+	"vertexColors" : "face"
+	}],
+
+    "vertices": [0.085699,-0.522346,0.155300,-0.115436,-0.015642,0.266847,-0.056635,-0.031215,-0.217772,0.038212,0.463273,-0.053121],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [-0.9729,-0.2131,-0.0899,-0.9846,0.1358,-0.1101],
+
+    "colors": [10684972],
+
+    "uvs": [[]],
+
+    "faces": [98,0,1,2,0,0,0,0,0,98,2,1,3,0,1,1,1,0]
+
+}
