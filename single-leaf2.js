88f73a77b53247e042df52cf08e4206f08145397
Baron Watts <watts.baron@hotmail.com>
2018-10-24 18:41:37 -0500

    Add files via upload

---
+++ single-leaf2.js
@@ -0,0 +1,46 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "single-leaf2.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 4,
+        "faces"         : 2,
+        "normals"       : 2,
+        "colors"        : 1,
+        "uvs"           : 4,
+        "materials"     : 1
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Material__25.001",
+	"colorAmbient" : [1.0, 1.0, 1.0],
+	"colorDiffuse" : [0.185098, 0.316863, 0.0],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 17.647059,
+	"transparency" : 1.0,
+	"vertexColors" : "face"
+	}],
+
+    "vertices": [-0.005393,0.057111,-0.000837,0.007761,0.032624,-0.005712,0.006514,-0.013870,0.007535,-0.009488,0.033229,0.011194],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [-0.6387,-0.1949,-0.7443,-0.7359,-0.1998,-0.647],
+
+    "colors": [3100672],
+
+    "uvs": [[0.3125,0.83,0.2825,0.9,0.3125,0.97,0.3425,0.9]],
+
+    "faces": [106,0,1,2,0,0,1,2,0,0,0,0,106,2,3,0,0,2,3,0,1,1,1,0]
+
+}
